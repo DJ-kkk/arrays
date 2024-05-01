cb6df2bb20b2c8a7d176ca69904f500faa5a09a2
DJ-kkk <129987327+DJ-kkk@users.noreply.github.com>
2024-05-01 20:01:01 +0100

    Add files via upload

---
+++ P154.py
@@ -0,0 +1,28 @@
+from  tkinter import *
+import random 
+
+root=Tk
+root.title("Testing Random Functions")
+root.geometry("400x400")
+
+array_3d = [[["I","J","K","L","M","N","O","P"],["King","Queen"],["!","@","#","£","%",">","&","*"]]]
+
+
+
+guessed _password_label["text"] = "Guessed Password: " + input_password.get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
